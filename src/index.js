2c64616a1d96adab8c29e42e5e3455f2d8ea47af
Arun Kumar Kushwaha <arunkushwaha_230604@aitpune.edu.in>
2025-11-13 00:30:52 +0530

    going for be production

--- src/index.js
+++ src/index.js
@@ -33,7 +33,11 @@ app.use((err, req, res) => {
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+    //listen for local development
+    if (process.env.NODE !== "production") {
+      app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    }
   } catch (error) {
     console.log("Failed to start server", error.message);
     process.exit(1);
@@ -41,3 +45,6 @@ const startServer = async () => {
 };
 
 startServer();
+
+//for production
+export default app;
